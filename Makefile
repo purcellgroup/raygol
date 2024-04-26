7982a512f1b38e30454eb024224201fbb2882602
9ziggy9 <davidarogers@protonmail.com>
2024-04-25 20:18:21 -0700

    draw checkerboard

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
 include custom.mk
 CC=gcc
-CFLAGS=-Wall -Wextra -pedantic -Wconversion -Wunreachable-code -Werror
+CFLAGS=-Wall -Wextra -pedantic -Wconversion -Wunreachable-code
 RLFLAGS=`pkg-config --cflags --libs raylib`
 EXE=./run
 
 all: clean main
-	$(call print_in_color,$(GREEN),\nRunning ...\n)
-	@$(EXE)
 
 main: main.c
 	$(call print_in_color,$(YELLOW),\nCompiling to $(EXE) ...\n)
--- main.c
+++ main.c
@@ -1,7 +1,38 @@
 #include <raylib.h>
+#include <stdlib.h>
+#include <string.h>
 #include "color.h"
 
+#define TARGET_FPS 30
+
+static int SQUARE_SIZE = 20;
+static int NUM_ROWS = 36;
+static int NUM_COLS = 54;
+
+void draw_board(void) {
+  for (int y = 0; y < NUM_ROWS; y++) {
+    for (int x = 0; x < NUM_COLS; x++) {
+      DrawRectangle(x * SQUARE_SIZE, y * SQUARE_SIZE,
+                    SQUARE_SIZE, SQUARE_SIZE,
+                    (x + y) % 2 ? DARKGRAY : LIGHTGRAY);
+    }
+  }
+}
+
 int main(void) {
-  COLOR_PRINT_256(223, 234, "Hello world!\n");
+  InitWindow(SQUARE_SIZE * NUM_COLS,
+             SQUARE_SIZE * NUM_ROWS,
+             "Conway's Game of Life");
+
+  SetTargetFPS(TARGET_FPS);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+      ClearBackground(DARKGRAY);
+      draw_board();
+    EndDrawing();
+  }
+
+  CloseWindow();
   return 0;
 }
